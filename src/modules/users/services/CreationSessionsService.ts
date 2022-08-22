339309635fb719f3fd67700818f3e01bcc7a0e99
uhzero <aureo.moraes@gmail.com>
2022-08-22 13:58:06 -0300

    add session authentication

---
+++ src/modules/users/services/CreationSessionsService.ts
@@ -0,0 +1,34 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppErrors';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import User from '../typeorm/entities/User';
+import { compare } from 'bcryptjs';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+// interface IResponse {
+//   user: User;
+// }
+
+class CreateSessionsService {
+  public async execute({ email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('Incorrect email or password combination!', 401);
+    }
+
+    const passwordConfirmed = await compare(password, user.password);
+
+    if (!passwordConfirmed) {
+      throw new AppError('Incorrect email or password combination!', 401);
+    }
+
+    return user;
+  }
+}
+
+export default CreateSessionsService;
