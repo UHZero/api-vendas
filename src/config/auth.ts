b716f728278b2d96e392a15949c112958e92a9c9
uhzero <aureo.moraes@gmail.com>
2022-08-22 16:16:23 -0300

    add JWT Token Authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '3c8a0c63e95c32a852e13a69e53c9f05',
+    expiresIn: '1d',
+  },
+};
--- src/modules/users/services/CreationSessionsService.ts
+++ src/modules/users/services/CreationSessionsService.ts
@@ -3,18 +3,21 @@ import AppError from '@shared/errors/AppErrors';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import User from '../typeorm/entities/User';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import authConfig from '@config/auth';
 
 interface IRequest {
   email: string;
   password: string;
 }
 
-// interface IResponse {
-//   user: User;
-// }
+interface IResponse {
+  user: User;
+  token: string;
+}
 
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
     if (!user) {
@@ -27,7 +30,12 @@ class CreateSessionsService {
       throw new AppError('Incorrect email or password combination!', 401);
     }
 
-    return user;
+    const token = sign({}, authConfig.jwt.secret, {
+      subject: user.id,
+      expiresIn: authConfig.jwt.expiresIn,
+    });
+
+    return { user, token };
   }
 }
 
