8944b6ce416ebf14f18ff43b719d1ca48e8a3f8c
uhzero <aureo.moraes@gmail.com>
2022-08-23 09:32:08 -0300

    include avatar controller and route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
+import avatarRouter from '@modules/users/routes/avatar.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/avatar', avatarRouter);
 
 export default routes;
